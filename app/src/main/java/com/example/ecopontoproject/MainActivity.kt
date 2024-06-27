804574e9f7967b6e3902d0b4e7319c7f7e465117
janicehortabd <janicebd2023@gmail.com>
2024-06-27 17:24:07 +0100

    Criar novas varives

--- app/src/main/java/com/example/ecopontoproject/MainActivity.kt
+++ app/src/main/java/com/example/ecopontoproject/MainActivity.kt
@@ -10,7 +10,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.ecopontoproject.ui.theme.EcoPontoProjectTheme
 
@@ -36,7 +41,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun EcoPonto(modifier: Modifier = Modifier) {
+    var page by remember { mutableStateOf(1) }
+    var option by remember { mutableStateOf(0) }
 
+
+    val message = when (option) {
+        1 -> stringResource(id = R.string.azul)
+        2 -> stringResource(id = R.string.amarelo)
+        3 -> stringResource(id = R.string.verde)
+        4 -> stringResource(id = R.string.laranja)
+        else -> "Vazio"
+
+    }
 }
 
 
